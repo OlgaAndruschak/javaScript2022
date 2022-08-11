905044152fb8dab400cb9742f3a77bd126159e96
OlgaAndruschak <olgaandrusak@gmail.com>
2022-08-11 22:43:57 +0300

    lesson1

---
+++ lesson1/lesson1.js
@@ -0,0 +1,49 @@
+let one = "hello";
+console.log(one);
+let two = "owu";
+console.log(two);
+let three = "com";
+console.log(three);
+let four = "ua";
+console.log(four);
+let five = 1;
+console.log(five);
+let six = 10;
+console.log(six);
+let seven = -999
+console.log(seven)
+let eight = 123
+console.log(eight)
+let nine = 3.14
+console.log(nine)
+const ten = 2.7
+console.log(ten)
+let eleven = 16
+console.log(eleven)
+let twelve = true
+console.log(twelve)
+let third = false
+console.log(third)
+alert('hello')
+// alert('owu')
+document.write('<div>' +one+ '</div>')
+
+let firstName = "Olga"
+let middleName = "Cute"
+let lastName = "Andruschak"
+let person = firstName+middleName+lastName
+console.log(person)
+
+let a = 100
+console.log(typeof a)
+let b = "100"
+console.log( typeof b)
+let c = true
+console.log( typeof c)
+
+let name = prompt('whats your name?')
+console.log(name)
+let surname = prompt('як тебе по батькові?')
+console.log(surname)
+let age = prompt('whats your age')
+console.log(age)
